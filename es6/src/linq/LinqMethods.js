be6f1ee80636483776d0bc686a04c907deb1622e
Antony Jones <antonyjones.1412@gmail.com>
2017-10-19 21:40:52 -0600

    Cleaned up a bunch of files + added node_modules to .gitignore

--- es6/src/linq/LinqMethods.js
+++
@@ -1,5 +0,0 @@
-export function* select(iterator, selector) {
-    for (let item of iterator) {
-        yield selector(item);
-    }
-}
--- es6/src/linq/select.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function* select(iterator, selector) {
-    for (let item of iterator) {
-        yield selector(item);
-    }
-}
-exports.select = select;
